36b604241a5359107f94e533e444b0958182655d
ramantv <90482664+ramantv@users.noreply.github.com>
2021-10-30 15:19:58 -0500

    Added Weather API calls

---
+++ assets/js/weatherdata.js
@@ -0,0 +1,40 @@
+const FORECAST_URL =
+  "https://api.openweathermap.org/data/2.5/onecall?lat=<LATITUDE>&lon=<LONGITUDE>&units=imperial&exclude=minutely,hourly&appid=<APIKEY>";
+const MY_API_KEY = "4fdcc0bfa551e7f510fce13137ab62cb";
+
+// To get a properly encoded API URL to pull the five day Forecast data for the supplied latitude and longitude.
+// This method takes the const API template URL for forecast and replaces the <LATITUDE> with the supplied latitide,
+// the <LONGITUDE> with the supplied longitude, and the <APIKEY> with my API key. Then encodes the URL properly so that
+// any spaces and/or special chars are handled properly.
+function getForecastAPIUrl(lat, lon) {
+  var url = FORECAST_URL;
+  var forecastAPIURL = encodeURI(
+    url
+      .replace("<LATITUDE>", lat)
+      .replace("<LONGITUDE>", lon)
+      .replace("<APIKEY>", MY_API_KEY)
+  );
+  //console.log("forecastAPIURL = " + forecastAPIURL);
+  return forecastAPIURL;
+}
+
+function getWeatherforLocation(lat, lon) {
+  ///5-day forecast API
+  var forecastUrl = getForecastAPIUrl(lat, lon);
+  console.log("API URL for Forecast: " + forecastUrl);
+
+  fetch(forecastUrl).then(function (forecastResp) {
+    if (forecastResp.ok) {
+      forecastResp
+        .json()
+        .then(function (foreData) {
+          console.log("5-day Forecast for " + city + ": ");
+          console.log(foreData);
+
+          cityWeatherData["forecastData"] = foreData;
+        })
+    } else {
+      alert("Weather Forecast Info not found for City: " + city);
+    }
+  });
+}
