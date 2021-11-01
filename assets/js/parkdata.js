4f634029b7291e0164d87d830ef4db70c9a6b60c
ramantv <90482664+ramantv@users.noreply.github.com>
2021-11-01 02:44:24 -0500

    Based on state chosen the park selector dropdown is built dynamically

--- assets/js/parkdata.js
+++ assets/js/parkdata.js
@@ -1,48 +1,57 @@
 var NPS_API_KEY = "Up5Uvv8A3J8sDp4LRjccKnWfqDHVRcbYRy3NBOPp";
-var parksInState_URL = 
-"https://developer.nps.gov/api/v1/parks?api_key=" + NPS_API_KEY +"&stateCode=";
+var parksInState_URL =
+  "https://developer.nps.gov/api/v1/parks?api_key=" +
+  NPS_API_KEY +
+  "&stateCode=";
 
+var parksData = [];
 
-// call the API to get the parks data
-/* 
 function getNationalParksforState(stateCode) {
+  // event.preventDefault();
 
-    var getParksURL = parksInState_URL + stateCode;
-    console.log("Going to fetch this URL: " + getParksURL);
-    fetch(parksInState_URL).then(function (response) {
-            //if response is okay, no errors found
-        if (response.ok) {
-            response.json().then(function (data) {
-                console.log("get Parks Data " + stateCode + ": " );
-                console.log(data);
-            }
-        });
+  //current conditions in user-entered city//using it to get long and latitude for One call weather API url
+  var getParksURL = parksInState_URL + stateCode;
+  console.log("Going to fetch this URL: " + getParksURL);
 
-    });
+  //make a request to the url
+  fetch(getParksURL).then(function (response) {
+    //if response is okay, no errors found
+    if (response.ok) {
+      response.json().then(function (data) {
+        console.log("get Parks Data for state of " + stateCode + ": ");
+        console.log(data);
+      });
+    } else {
+      //if city name is invalid return error message
+      alert("Data not found for State! ");
+    }
+  });
 }
- */
 
-function getNationalParksforState(stateCode) {
-    // event.preventDefault();
-  
-    //current conditions in user-entered city//using it to get long and latitude for One call weather API url
-    var getParksURL = parksInState_URL + stateCode;
-    console.log("Going to fetch this URL: " + getParksURL);
-  
-    //make a request to the url
-    fetch(getParksURL).then(function (response) {
-      //if response is okay, no errors found
-      if (response.ok) {
-          response.json().then(function (data) {
-              console.log("get Parks Data for state of " + stateCode + ": " );
-              console.log(data);
-          });
-      } 
-      else {
-          //if city name is invalid return error message
-        alert("Data not found for State! ");
-      }
-    });
+async function fetchNPsForState(stateCode) {
+  //current conditions in user-entered city//using it to get long and latitude for One call weather API url
+  var getParksURL = parksInState_URL + stateCode;
+  console.log("Going to fetch this URL: " + getParksURL);
+
+  let response = await fetch(getParksURL);
+  if (!response.ok) {
+    var message = `An error has occured: ${response.status}`;
+    throw new Error(message);
   }
 
-  
+  var parks = await response.json();
+  console.log(parks);
+
+  return parks;
+}
+
+function getParks(state) {
+  fetchNPsForState(state)
+    .then((parks) => {
+      parksData = parks; // fetched NP data
+    })
+    .catch((error) => {
+      error.message; //
+    });
+    return parksData;
+}
---
+++ assets/js/script.js
@@ -0,0 +1,53 @@
+var burgerEl = document.querySelector(".burger");
+var homeBtnEl = document.querySelector("#home-button");
+
+var pageHeadingEl = document.querySelector("#page-heading");
+
+var bodySectionEl = document.querySelector("#body-section");
+var stateSectionEl = document.querySelector("#state-section");
+var parkSelectorEl = document.querySelector("#park-selector");
+
+function onStateSelectionChange() {
+  var stateCode = $(this).val();
+  var stateName = $(this).find("option:selected").text();
+  pageHeadingEl.innerHTML = "Explore " + stateName;
+
+  bodySectionEl.classList.toggle("is-hidden");
+  stateSectionEl.classList.toggle("is-hidden");
+
+  fetchNPsForState(stateCode)
+    .then((parksInState) => {
+      console.log("Number of Parks in " + stateName + " " + parksInState.data.length);
+      buildParksSelector(parksInState.data);
+      parkSelectorEl.classList.toggle("is-hidden");
+    })
+    .catch((err) => {
+      console.log("Error: ", err.message);
+    });
+}
+
+function buildParksSelector(pd) {
+    $('#national-park').empty();
+    $.each(pd, function (i, park) {
+        $('#national-park').append($('<option>', { 
+            value: park.parkCode,
+            text : park.fullName 
+        }));
+    });
+
+}
+
+function onTopMenuButtonClick() {
+  window.location.reload();
+}
+
+$(document).ready(function () {
+  var pageHeading = "Explore National Parks";
+  pageHeadingEl.innerHTML = pageHeading;
+
+  // Home button click handler.
+  $("#home-button").on("click", onTopMenuButtonClick);
+
+  // States dropdown handler
+  $("#state").on("change", onStateSelectionChange);
+});
