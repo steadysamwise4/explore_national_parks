aa08d1756fbcc1a7db3e2c7ef813cfe219b316f0
ramantv <90482664+ramantv@users.noreply.github.com>
2021-11-02 02:35:27 -0500

    All Park details dynamically displayed upon park selection. Styling updated.

--- assets/js/parkdata.js
+++ assets/js/parkdata.js
@@ -4,8 +4,6 @@ var parksInState_URL =
   NPS_API_KEY +
   "&stateCode=";
 
-var parksData = [];
-
 function getNationalParksforState(stateCode) {
   // event.preventDefault();
 
@@ -45,13 +43,19 @@ async function fetchNPsForState(stateCode) {
   return parks;
 }
 
-function getParks(state) {
-  fetchNPsForState(state)
-    .then((parks) => {
-      parksData = parks; // fetched NP data
-    })
-    .catch((error) => {
-      error.message; //
-    });
-    return parksData;
-}
+function formatPhoneNumber(str) {
+    //Filter only numbers from the input
+    let cleaned = ('' + str).replace(/\D/g, '');
+    
+    //Check if the input is of correct
+    let match = cleaned.match(/^(1|)?(\d{3})(\d{3})(\d{4})$/);
+    
+    if (match) {
+      //Remove the matched extension code
+      //Change this to format for any country code.
+      let intlCode = (match[1] ? '+1 ' : '');
+      return [intlCode, '(', match[2], ') ', match[3], '-', match[4]].join('');
+    }
+    
+    return "";
+  }
--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,8 @@ var bodySectionEl = document.querySelector("#body-section");
 var stateSectionEl = document.querySelector("#state-section");
 var parkSelectorEl = document.querySelector("#park-selector");
 
+var parks = []; // to hold the data returned from the NPS API call
+
 function onStateSelectionChange() {
   var stateCode = $(this).val();
   var stateName = $(this).find("option:selected").text();
@@ -17,7 +19,11 @@ function onStateSelectionChange() {
 
   fetchNPsForState(stateCode)
     .then((parksInState) => {
-      console.log("Number of Parks in " + stateName + " " + parksInState.data.length);
+      console.log(
+        "Number of Parks in " + stateName + " " + parksInState.data.length
+      );
+
+      parks = parksInState;
       buildParksSelector(parksInState.data);
       parkSelectorEl.classList.toggle("is-hidden");
     })
@@ -27,14 +33,120 @@ function onStateSelectionChange() {
 }
 
 function buildParksSelector(pd) {
-    $('#national-park').empty();
-    $.each(pd, function (i, park) {
-        $('#national-park').append($('<option>', { 
-            value: park.parkCode,
-            text : park.fullName 
-        }));
-    });
+  $("#national-park").empty();
 
+  $("#national-park").append(
+    $("<option>", {
+      value: -1,
+      text: "Select a Park...",
+    })
+  );
+
+  $.each(pd, function (i, park) {
+    $("#national-park").append(
+      $("<option>", {
+        value: i,
+        text: park.fullName,
+      })
+    );
+  });
+}
+
+function onParkSelectionChange() {
+  var idx = $(this).val();
+  var name = $(this).find("option:selected").text();
+  console.log("Selected Park Index = " + idx);
+  console.log("Selected Park Name = " + name);
+
+  $("#park-name").html(parks.data[idx].fullName);
+
+  $("#park-description").html(parks.data[idx].description);
+
+  $("#park-pic").attr("src", parks.data[idx].images[0].url);
+
+  $("#park-address").html(getParkAddress(parks.data[idx].addresses));
+
+  var parkEmail = getParkEmail(parks.data[idx].contacts);
+  $("#park-email").html(parkEmail);
+
+  var parkMapUrl = getMapLink(
+    parks.data[idx].latitude,
+    parks.data[idx].longitude
+  );
+  $("#park-map-link").attr("href", parkMapUrl);
+
+  $("#park-hours").html(getParkHours(parks.data[idx].operatingHours));
+
+  var parkPhone = getParkPhone(parks.data[idx].contacts);
+  $("#park-phone").html(parkPhone);
+  $("#park-website-link").attr("href", parks.data[idx].url);
+
+  var parkActivities = getParkActivities(parks.data[idx].activities);
+  $("#park-activities").html(parkActivities);
+
+  $("#park-designation").html(parks.data[idx].designation);
+}
+
+function getParkEmail(contacts) {
+  var email = contacts.emailAddresses[0].emailAddress;
+  return email;
+}
+
+function getParkPhone(contacts) {
+  var phone = formatPhoneNumber(contacts.phoneNumbers[0].phoneNumber);
+  if (contacts.phoneNumbers[0].extension) {
+    phone += " x" + contacts.phoneNumbers[0].extension;
+  }
+  return phone;
+}
+
+function getParkHours(hours) {
+  var hoursString = hours[0].description + "<br>";
+  hoursString += "Standard Hours: <br>";
+  hoursString += JSON.stringify(hours[0].standardHours) + "<br>";
+  hoursString +=
+    "For closures/exceptions visit National Park Website." + "<br>";
+  return hoursString;
+}
+
+function getParkActivities(acts) {
+  var parkActs = "";
+  for (let i = 0; i < acts.length; i++) {
+      parkActs += ((parkActs)?(", " + acts[i].name ):(acts[i].name ));
+  }
+  return parkActs;
+}
+
+function getParkAddress(addresses) {
+  let i = 0;
+  var addr = "";
+  console.log(addresses);
+
+  while (i < addresses.length) {
+    if (addresses[i].type === "Physical") {
+      addr += addresses[i].line1 + "<br>";
+      if (addresses[i].line2) {
+        addr += addresses[i].line2 + "<br>";
+      }
+      if (addresses[i].line3) {
+        addr += addresses[i].line3 + "<br>";
+      }
+      addr +=
+        addresses[i].city +
+        " " +
+        addresses[i].stateCode +
+        " " +
+        addresses[i].postalCode +
+        "<br>";
+      break;
+    }
+  }
+  console.log(addr);
+  return addr;
+}
+
+function getMapLink(lat, lon) {
+  return encodeURI("http://maps.google.com/maps?q=" + lat + "," + lon + "&t=h");
 }
 
 function onTopMenuButtonClick() {
@@ -50,9 +162,10 @@ $(document).ready(function () {
 
   // States dropdown handler
   $("#state").on("change", onStateSelectionChange);
+
+  // States dropdown handler
+  $("#national-park").on("change", onParkSelectionChange);
 });
-
-
 
 /*
 http://maps.google.com/maps?q=38.44023613,-96.5670822&t=h
