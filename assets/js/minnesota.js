18b9eb569373bea5b8e411573208a07fc74a5329
Sam Davenport <samueldavenport@att.net>
2021-11-01 23:11:50 -0500

    minnesota page now displays data for each park upon the click of that park link. It also displays current weather data

--- assets/js/minnesota.js
+++ assets/js/minnesota.js
@@ -12,6 +12,11 @@ var errorEl = document.querySelector(".error");
 
 var parkListEl = document.querySelector(".park-list");
 var parkDataEl = document.querySelector("#park-link");
+var heroPicEl = document.querySelector(".hero");
+var mainHeadingEl = document.querySelector(".top");
+var currentWeatherEl = document.querySelector(".weather");
+
+
 
 
 var buttonClickHandler = function(event) {
@@ -73,6 +78,16 @@ var getParksData = function(park) {
 var displayParkData = function(nps, i) {
     // clear old
     parkDataEl.innerHTML = "";
+    heroPicEl.innerHTML = "";
+    mainHeadingEl.innerHTML = "";
+
+    // heading
+    var heading = nps.data[i].fullName;
+    var headingEl = document.createElement('p');
+    headingEl.classList = "title park-title";
+    headingEl.textContent = heading;
+    parkDataEl.appendChild(headingEl);
+
      
     // add image to the dom
     var image = nps.data[i].images[0].url;
@@ -83,10 +98,103 @@ var displayParkData = function(nps, i) {
     figureEl.appendChild(imgEl);
     parkDataEl.appendChild(figureEl);
 
+    // caption
+    var caption = nps.data[i].images[0].caption;
+    var captionEl = document.createElement('p');
+    captionEl.classList = "caption";
+    captionEl.textContent = caption;
+    parkDataEl.appendChild(captionEl);
 
-    
+    // description
+    var descript = nps.data[i].description;
+    var descriptEl = document.createElement("p");
+    descriptEl.classList = "description";
+    descriptEl.textContent = descript;
+    parkDataEl.appendChild(descriptEl);
 
+    // cost
+    var cost = nps.data[i].entranceFees[0].cost;
+    var costEl = document.createElement('h3');
+    costEl.classList = "info";
+    costEl.textContent = "Cost: $" + cost;
+    parkDataEl.appendChild(costEl);
 
+    // address
+    var street = nps.data[i].addresses[0].line1;
+    var city = nps.data[i].addresses[0].city;
+    var state = nps.data[i].addresses[0].stateCode;
+    var address = street + " - " + city + ", " + state;
+    var addressEl = document.createElement("p")
+    addressEl.classList = "info";
+    addressEl.textContent = "Located at: " + address;
+    parkDataEl.appendChild(addressEl);
+
+    // more room for activities
+    var activeHeading = document.createElement("h4");
+    activeHeading.classList = "info"
+    activeHeading.textContent = "Activities include:"
+    parkDataEl.appendChild(activeHeading);
+    var activitiesEl = document.createElement("p");
+    activitiesEl.classList = "activities";
+    var activArr = nps.data[i].activities;
+    var activityListArr = [];
+    activArr.forEach(function(obj){
+    activityListArr.push(obj.name);
+    })
+    activitiesEl.textContent = JSON.stringify(activityListArr);
+    parkDataEl.appendChild(activitiesEl);
+
+    // weather info
+    var weatherHeading = document.createElement("h4");
+    weatherHeading.classList = "info"
+    weatherHeading.textContent = "Weather info:"
+    parkDataEl.appendChild(weatherHeading);
+    var weather = nps.data[i].weatherInfo;
+    var weatherEl = document.createElement("p");
+    weatherEl.classList = "activities";
+    weatherEl.textContent = weather;
+    parkDataEl.appendChild(weatherEl);
+
+    // format latitude and longitude
+    var lat = nps.data[i].latitude;
+    var lon = nps.data[i].longitude;
+
+    // current weather
+    // var currentWeather = 
+    // var currentWeatherEl = document.createElement("p")
+
+    getForecastData(lat, lon);
+}
+
+var getForecastData = function(latitude, longitude) {
+    //format the open weather api forcast url
+    var apiForecast = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&exclude=minutely,hourly,alerts&units=imperial&appid=9ac94d5206a0a04e92ba7cbf64fe39f8";
+    // make the request to the url
+    fetch(apiForecast).then(function(response) {
+        response.json().then(function(data) {
+            displayForecastData(data);
+        });
+    });
+}
+
+ var displayForecastData = function(weather) {
+    console.log(weather);
+    var currentWeatherHeading = "Current Conditions:"
+    var currentWeatherHeadingEl = document.createElement("h4");
+    currentWeatherHeadingEl.classList = "info";
+    currentWeatherHeadingEl.textContent = currentWeatherHeading;
+    currentWeatherEl.appendChild(currentWeatherHeadingEl);
+    var weatherDescript = weather.current.weather[0].description;
+    var weatherDescriptEl = document.createElement("p")
+    weatherDescriptEl.classList = "activities";
+    weatherDescriptEl.textContent = weatherDescript;
+    currentWeatherEl.appendChild(weatherDescriptEl);
+
+    var temp = Math.round(weather.current.temp);
+    var tempEl = document.createElement("p");
+    tempEl.classList = "activities";
+    tempEl.textContent = "Currently: " + temp + "°F";
+    currentWeatherEl.appendChild(tempEl);
 }
 
 getParkNames();
