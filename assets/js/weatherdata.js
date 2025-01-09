db2f7b0027e96929aaaeecfff91ef6233bee6093
steadysamwise4 <davenportsam44@gmail.com>
2025-01-09 10:35:48 -0600

    small fixes

--- assets/js/weatherdata.js
+++ assets/js/weatherdata.js
@@ -1,6 +1,6 @@
 const FORECAST_URL =
-  "https://api.openweathermap.org/data/2.5/onecall?lat=<LATITUDE>&lon=<LONGITUDE>&units=imperial&exclude=minutely,hourly&appid=<APIKEY>";
-const MY_API_KEY = "4fdcc0bfa551e7f510fce13137ab62cb";
+  'https://api.openweathermap.org/data/3.0/onecall?lat=<LATITUDE>&lon=<LONGITUDE>&units=imperial&exclude=minutely,hourly&appid=<APIKEY>';
+const MY_API_KEY = '2e140310cf16305eb4c4a092c0d9247c';
 
 // To get a properly encoded API URL to pull the five day Forecast data for the supplied latitude and longitude.
 // This method takes the const API template URL for forecast and replaces the <LATITUDE> with the supplied latitide,
@@ -10,9 +10,9 @@ function getForecastAPIUrl(lat, lon) {
   var url = FORECAST_URL;
   var forecastAPIURL = encodeURI(
     url
-      .replace("<LATITUDE>", lat)
-      .replace("<LONGITUDE>", lon)
-      .replace("<APIKEY>", MY_API_KEY)
+      .replace('<LATITUDE>', lat)
+      .replace('<LONGITUDE>', lon)
+      .replace('<APIKEY>', MY_API_KEY)
   );
   //console.log("forecastAPIURL = " + forecastAPIURL);
   return forecastAPIURL;
@@ -21,29 +21,26 @@ function getForecastAPIUrl(lat, lon) {
 function getWeatherforLocation(lat, lon) {
   ///5-day forecast API
   var forecastUrl = getForecastAPIUrl(lat, lon);
-  console.log("API URL for Forecast: " + forecastUrl);
+  console.log('API URL for Forecast: ' + forecastUrl);
 
   fetch(forecastUrl).then(function (forecastResp) {
     if (forecastResp.ok) {
-      forecastResp
-        .json()
-        .then(function (foreData) {
-          console.log("5-day Forecast for " + city + ": ");
-          console.log(foreData);
+      forecastResp.json().then(function (foreData) {
+        console.log('5-day Forecast for ' + city + ': ');
+        console.log(foreData);
 
-          cityWeatherData["forecastData"] = foreData;
-        })
+        cityWeatherData['forecastData'] = foreData;
+      });
     } else {
-      alert("Weather Forecast Info not found for City: " + city);
+      alert('Weather Forecast Info not found for City: ' + city);
     }
   });
 }
 
-
 async function fetchWeatherForLocation(lat, lon) {
   ///5-day forecast API
   var forecastUrl = getForecastAPIUrl(lat, lon);
-  console.log("Going to fetch this URL for Forecast: " + forecastUrl);
+  console.log('Going to fetch this URL for Forecast: ' + forecastUrl);
 
   let response = await fetch(forecastUrl);
   if (!response.ok) {
